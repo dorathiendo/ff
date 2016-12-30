9ba97d9b09831e1889c6915bd9e1d8e46adcd15d
m652326 <Dora.Do@macys.com>
2016-12-30 12:55:54 -0800

    commit

--- routes/news.js
+++ routes/news.js
@@ -6,15 +6,16 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'cungleme',
-  database: "fleetfoxes"
+  database: "fleetfoxes",
+  dateStrings: 'date'
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM posts', function (err, rows, fields) {
+  connection.query('SELECT * FROM posts ORDER BY date DESC', function (err, rows, fields) {
     if (err) throw err
     _.each(rows, function(row){
-      row.date = row.date.toDateString();
+      row.date = new Date(row.date).toDateString();
     });
     res.render('news', { title: 'News', posts: rows });
   });
@@ -24,17 +25,24 @@ router.post('/', function (req, res) {
   switch(req.body.type){
     case 'text':
       req.body.content = '<p>' + req.body.content + '</p>';
+          break;
+    case 'video':
+      req.body.content = '';
+      break;
+    case 'image':
+      req.body.content = '';
   }
+  var date = new Date();
   var params = {
     type: req.body.type,
-    date: req.body.date,
+    date: date,
     title: req.body.title,
     content: req.body.content
   };
   connection.query('INSERT INTO posts SET ?', params, function (err, rows) {
     if(err){
       res.status(400);
-      res.send('Error creating post');
+      res.send(err);
     } else {
       res.status(200);
       res.send('Post created');
